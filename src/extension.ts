fbfeebdcc617e67be6d4f1064ca5c26d30c0cf6e
JustifiedFlaw <ebbytes@gmail.com>
2022-01-14 08:12:02 -0500

    basic first version

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,57 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "clipcycle" is now active!');
+	var history: string[] = [];
+	var position: number = -1;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('clipcycle.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Clipcycle!');
-	});
+	function historyPush(content: string) {
+		history.push(content);
+		while (history.length > 10) {
+			history.shift();
+		}
+		position = history.length - 1;
+	}
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('clipcycle.copy', () => {
+		vscode.commands.executeCommand("editor.action.clipboardCopyAction").then(() => {
+			vscode.env.clipboard.readText().then(content => {
+				historyPush(content);
+			});	
+		});	
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('clipcycle.cut', () => {
+		vscode.commands.executeCommand("editor.action.clipboardCutAction").then(() => {
+			vscode.env.clipboard.readText().then(content => {
+				historyPush(content);
+			});	
+		});	
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('clipcycle.cyclePaste', async () => {
+		let editor = vscode.window.activeTextEditor;
+		if (history.length > 0 && editor) {
+			position = position >= 0 && position < history.length ? position : history.length - 1;
+			var last = history[position];
+			if (last) {	
+				await vscode.env.clipboard.writeText(last);
+				
+				await vscode.commands.executeCommand("editor.action.clipboardPasteAction");
+
+				editor.selections = editor.selections.map(s => new vscode.Selection(
+					s.anchor.line,
+					s.anchor.character - last.length,
+					s.anchor.line,
+					s.anchor.character));
+			}
+			position--;
+		}
+		else {
+			vscode.commands.executeCommand("editor.action.clipboardPasteAction");
+		}
+	}));
+
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
